87f02f8b944db9bf5c2f15dcd092f444940c0d15
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-07 23:29:09 -0800

    fixed run-time bug

--- random_sentence.py
+++ random_sentence.py
@@ -3,11 +3,11 @@ import time
 import sys
 
 def random_sentence(number):
-    return "this is a random sentence"
+    
 
 if __name__ == '__main__':
     t0 = time.time()
     num_of_words = int(sys.argv[1])
-    sentence = create_sentence(num_of_words)
+    sentence = random_sentence(num_of_words)
     t1 = time.time()
     print(t1- t0)
