edfe51da2a3bc62074352cb8918107e03498fe76
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-07 23:33:55 -0800

    function forms an array of all words from the built-in dictionary

--- random_sentence.py
+++ random_sentence.py
@@ -3,7 +3,10 @@ import time
 import sys
 
 def random_sentence(number):
-    
+    f = open("/usr/share/dict/words")
+    l = f.readlines()
+    lines = [s.strip('\n') for s in l]
+    print lines
 
 if __name__ == '__main__':
     t0 = time.time()
