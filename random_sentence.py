b70cae218ae0ba92517a450922cb68fa05080e44
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-08 23:29:05 -0800

    got function to spit random word from dictionary

--- random_sentence.py
+++ random_sentence.py
@@ -4,7 +4,9 @@ def random_sentence():
     f = open("/usr/share/dict/words")
     l = f.readlines()
     lines = [s.strip('\n') for s in l]
-    print lines
+    length = len(lines)
+    rand_index = random.randint(0, length)
+    print lines[rand_index]
 
 if __name__ == '__main__':
     random_sentence()
