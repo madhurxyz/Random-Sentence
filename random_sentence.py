6163d775dfe05b27499d31530c75be9cbbdca473
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-08 23:31:21 -0800

    got function to spit array of random words of any length

--- random_sentence.py
+++ random_sentence.py
@@ -1,12 +1,16 @@
 import random
 
-def random_sentence():
+def random_sentence(number):
     f = open("/usr/share/dict/words")
     l = f.readlines()
     lines = [s.strip('\n') for s in l]
     length = len(lines)
-    rand_index = random.randint(0, length)
-    print lines[rand_index]
+    word_list = []
+    for index in range(0, number):
+        rand_index = random.randint(0, length)
+        word_list.append(lines[rand_index])
+    print word_list
+    return word_list
 
 if __name__ == '__main__':
-    random_sentence()
+    random_sentence(5)
